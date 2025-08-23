be10127067950137f1c3101b9fd66198fc793775
MaferGH <mafergaria@gmail.com>
2025-08-22 19:04:29 -0600

    nullability

---
+++ bin/nulabilidad.dart
@@ -0,0 +1,17 @@
+import 'dart:ffi';
+import 'dart:io';
+
+//Un string vacio contiene una lista de caracteres vacio, la nula no contiene absolutamente nada y si intentas acceder a algo nulo este revienta
+
+void main(List<String> arguments) {
+
+}
+
+void nullability(){
+//Para poder asignar un null a una variable de cualquier tipo es agregandole el sigo ?
+
+String? name = 'Fer';
+name = null;
+
+// Nota: es importante tener un null por si el usuario no pone nada, esto es para aplicacion de flutter
+}
